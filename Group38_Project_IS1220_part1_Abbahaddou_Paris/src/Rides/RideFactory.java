5fa97320a8d1b99d4ed5249db4a84793c4925ab3
Antoine <antoine.paris@student.ecp.fr>
2018-11-12 18:32:42 +0100

    RideFactory

--- Group38_Project_IS1220_part1_Abbahaddou_Paris/src/Rides/RideFactory.java
+++ Group38_Project_IS1220_part1_Abbahaddou_Paris/src/Rides/RideFactory.java
@@ -1,7 +1,24 @@
 package Rides;
 
+import java.sql.Time;
+
+import GPS.GPScoordinates;
+import customersAndDrivers.Customer;
+
 //kind of Ride Factory but with more functionality (
 public class RideFactory {
+	GPScoordinates startingPoint;
+	GPScoordinates endingPoint;
+	Time time;
+	Customer cust;
 	
+	public Ride create(String nameOfRide) {
+		if (nameOfRide.equals("uberX")) {
+			return new UberX(null, cust, startingPoint, endingPoint, time)
+		}
+		
+		return null;
+		
+	}
 
 }
